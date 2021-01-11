7d237d9fbe6d7580fb1ff02f7ae0c0483f6a5354
Ben <benjamin.main88@gmail.com>
2021-01-11 12:22:59 +0000

    State is updated for each form field based on input

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,18 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const CurrencyConverter = () => {
-    useEffect(() => {
-        axios({
-            method: "GET",
-            url:
-                "https://free.currconv.com/api/v7/convert?q=USD_GBP&compact=ultra&apiKey=0940afb0a07457dbd415",
-        }).then((response) => {
-            console.log(response);
-        });
-    });
+    const [first, setFirst] = useState("");
+    const [second, setSecond] = useState("");
 
-    return <h2>Did this work</h2>;
+    // useEffect(() => {
+    //     axios({
+    //         method: "GET",
+    //         url:
+    //             "https://free.currconv.com/api/v7/convert?q=USD_GBP&compact=ultra&apiKey=0940afb0a07457dbd415",
+    //     }).then((response) => {
+    //         console.log(response.data);
+    //     });
+    // });
+
+    const getRate = () => {};
+
+    const handleChangeFirst = (e) => setFirst(e.currentTarget.value);
+
+    const handleChangeSecond = (e) => setSecond(e.currentTarget.value);
+
+    return (
+        <section>
+            <h2>Currency Pair</h2>
+            <form onSubmit={getRate}>
+                <label>First</label>
+                <input onChange={handleChangeFirst} value={first}></input>
+                <label>Second</label>
+                <input onChange={handleChangeSecond} value={second}></input>
+                <button>Get Rate</button>
+            </form>
+        </section>
+    );
 };
 
 export default CurrencyConverter;
