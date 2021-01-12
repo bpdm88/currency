0d041b57f4985c377d1b136ace3a60b343d912c9
Ben <benjamin.main88@gmail.com>
2021-01-12 10:12:00 +0000

    data object set as state for list

--- src/components/LatestRates.js
+++ src/components/LatestRates.js
@@ -1,18 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const LatestRates = () => {
+    const [list, setList] = useState();
+
     useEffect(() => {
         axios({
             method: "GET",
             url:
-                "https://v6.exchangerate-api.com/v6/b2ff8db2977c244d5c41f000/latest/USD",
+                "https://v6.exchangerate-api.com/v6/b2ff8db2977c244d5c41f000/latest/GBP",
         }).then((response) => {
             console.log(response.data.conversion_rates);
+            setList(response.data.conversion_rates);
         });
-    });
+    }, []);
 
-    return <h2>Hello again</h2>;
+    const test = () => {
+        console.log(list.AED);
+    };
+
+    return <button onClick={test}>Click</button>;
 };
 
 export default LatestRates;
